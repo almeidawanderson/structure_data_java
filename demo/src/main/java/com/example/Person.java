f624e5cba2435081475cf45362d8b1656ed84246
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-07-20 09:23:06 -0400

    introduce all structure data with java

---
+++ demo/src/main/java/com/example/Person.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private String cpf;
+    private String rg;
+    private String address;
+    private String phone;
+    private String email;
+    
+    public Person(String name, int age, String cpgf, String rg, String address, String phone, String email){
+        this.name = name;
+        this.age = age;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }  
+
+    Person person1 = new Person(name, age, cpf, rg, address, phone, email);
+
+   
+    
+
+
+    }
+    
+
